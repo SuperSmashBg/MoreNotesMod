d82d132c30ca1ef12285aa266f1e2ef5f5700d50
Ben G <Ben.goliak@gmail.com>
2023-12-06 18:19:57 -0500

    Add project files.

---
+++ Patches/TestPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace MoreNotesMod.Patches
+{
+    [HarmonyPatch]
+    public class WirtePatch
+    {
+        [HarmonyPatch(typeof(StartOfRound), "WritePlayerNotes")]
+        [HarmonyPostfix]
+        public static void MyPatch(StartOfRound __instance)
+        {
+            for (int i = 0; i < __instance.gameStats.allPlayerStats.Length; i++)
+            {
+                if (__instance.gameStats.allPlayerStats[i].isActivePlayer && !__instance.allPlayerScripts[i].isPlayerDead)
+                {
+                    __instance.gameStats.allPlayerStats[i].playerNotes.Add($"Took {__instance.gameStats.allPlayerStats[i].stepsTaken} steps");
+                } 
+            } 
+            
+        }
+        
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace MoreNotesMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Harmony _harmony;
+        //public ManualLogSource mls;
+        public static Plugin Instance;
+        private void Awake()
+        {
+            //Netcode shit
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                foreach (var method in methods)
+                {
+                    var attributes = method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
+                    if (attributes.Length > 0)
+                    {
+                        method.Invoke(null, null);
+                    }
+                }
+            }
+            
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded! And the shortcut works perfect");
+
+    
+            _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            _harmony.PatchAll(typeof(Patches.WirtePatch));
+
+        }
+    }
+}
