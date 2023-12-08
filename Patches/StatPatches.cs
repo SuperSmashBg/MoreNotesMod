f2282fb7b2d2ad8edacedf4a26af49806346bd6c
Ben G <Ben.goliak@gmail.com>
2023-12-08 17:49:47 -0500

    Refactor to prefix

--- Patches/StatPatches.cs
+++ Patches/StatPatches.cs
@@ -1,6 +1,8 @@
 ﻿using GameNetcodeStuff;
 using HarmonyLib;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MoreNotesMod.Patches
 {
@@ -9,6 +11,7 @@ namespace MoreNotesMod.Patches
     {
 
         public static float[] danceTotal;
+        public static int conPlayerReq = 0;
 
         [HarmonyPatch(typeof(StartOfRound), "StartGame")]
         [HarmonyPostfix]
@@ -18,36 +21,96 @@ namespace MoreNotesMod.Patches
            danceTotal = new float[__instance.gameStats.allPlayerStats.Length];
            
         }
+
         //Patches after the WritePlayerNotes method
         [HarmonyPatch(typeof(StartOfRound), "WritePlayerNotes")]
-        [HarmonyPostfix]
-        public static void WrirtePlayerNotesPatch(StartOfRound __instance)
+        [HarmonyPrefix]
+        public static bool WrirtePlayerNotesPatch(StartOfRound __instance)
         {
-            //Loop through all players and display their step count
-            for (int i = 0; i < __instance.gameStats.allPlayerStats.Length; i++) 
+            //Holds all of the data related to player notes
+            List<Tuple<int, string>> playerNoteStorage = new List<Tuple<int, string>>();
+            
+            //Quick ref to an important value
+            var q_allPlayerStats =  __instance.gameStats.allPlayerStats;
+            var q_allPlayerScripts = __instance.allPlayerScripts;
+
+            //Refactor of the code that sets isActivePlayer
+            for (int i = 0; i < q_allPlayerStats.Length; i++)
             {
-                if (__instance.gameStats.allPlayerStats[i].isActivePlayer && !__instance.allPlayerScripts[i].isPlayerDead)
-                {
-                    __instance.gameStats.allPlayerStats[i].playerNotes.Add($"Took {__instance.gameStats.allPlayerStats[i].stepsTaken} steps"); //Adds step count
-                }
+                q_allPlayerStats[i].isActivePlayer =
+                    (q_allPlayerScripts[i].disconnectedMidGame
+                    || q_allPlayerScripts[i].isPlayerDead
+                    || q_allPlayerScripts[i].isPlayerControlled);
             }
 
-            //Display best dancer
-            float maxDance = 0;
-            int maxDanceId = 0;
-
-            for (int i = 0; i < __instance.gameStats.allPlayerStats.Length; i++)
+            //Refactor of code that gets the min step player
             {
-
-                if (__instance.gameStats.allPlayerStats[i].isActivePlayer && !__instance.allPlayerScripts[i].isPlayerDead 
-                    && danceTotal[i] >= maxDance)
-                {
-                    maxDance = danceTotal[i];
-                    maxDanceId = i;
-                }
+                int minSteps = q_allPlayerStats
+                    .Where(stats => stats.isActivePlayer).ToArray()
+                    .Select(stats => stats.stepsTaken).ToArray()
+                    .Min();
+                int minStepsId = Array.IndexOf(q_allPlayerStats, q_allPlayerStats.First(stats => stats.stepsTaken == minSteps));
+                if (__instance.connectedPlayersAmount > conPlayerReq && minSteps < 10)
+                    playerNoteStorage.Add(new Tuple<int, string>(minStepsId, "The laziest employee."));
             }
 
-            __instance.gameStats.allPlayerStats[maxDanceId].playerNotes.Add($"Hit the dance floor for {maxDance:0} seconds");
+            //Refactor of code that gets the max turns player
+            {
+                int maxTurns = q_allPlayerStats
+                    .Where((stats) => stats.isActivePlayer).ToArray()
+                    .Select((stats) => stats.turnAmount).ToArray()
+                    .Max();
+                int maxTurnsId = Array.IndexOf(q_allPlayerStats, q_allPlayerStats.First(stats => stats.turnAmount == maxTurns));
+
+                if (__instance.connectedPlayersAmount > conPlayerReq)
+                    playerNoteStorage.Add(new Tuple<int, string>(maxTurnsId, "The most paranoid employee."));
+            }
+
+            //Refactor of code that gets the most injured
+            {
+                int maxDamageTaken = q_allPlayerStats
+                    .Where((stats) => stats.isActivePlayer).ToArray()
+                    .Select((stats) => stats.damageTaken).ToArray()
+                    .Max();
+                int maxDamageTakenId = Array.IndexOf(q_allPlayerStats, q_allPlayerStats.First(stats => stats.damageTaken == maxDamageTaken));
+
+                if (__instance.connectedPlayersAmount > conPlayerReq)
+                    playerNoteStorage.Add(new Tuple<int, string>(maxDamageTakenId, "Sustained the most injuries."));
+            }
+
+            //Refactor of code that gets the most injured
+            {
+                int maxProfit = q_allPlayerStats
+                    .Where((stats) => stats.isActivePlayer).ToArray()
+                    .Select((stats) => stats.profitable).ToArray()
+                    .Max();
+                int maxProfitId = Array.IndexOf(q_allPlayerStats, q_allPlayerStats.First(stats => stats.profitable == maxProfit));
+
+                if (__instance.connectedPlayersAmount > conPlayerReq && maxProfit > 50)
+                    playerNoteStorage.Add(new Tuple<int, string>(maxProfitId, "Most profitable."));
+            }
+
+            //Adds best dancer
+            {
+                float maxDance = danceTotal
+                    .Where((stats, index) => q_allPlayerStats[index].isActivePlayer).ToArray() //Where player is active
+                    .Max(); //Get Max
+                int maxDanceId = Array.IndexOf(danceTotal, maxDance);
+
+                //Player must be dancing for so long
+                if (__instance.connectedPlayersAmount > conPlayerReq && maxDance >= 20f)
+                    playerNoteStorage.Add(new Tuple<int, string>(maxDanceId, $"Hit the dance floor for {maxDance:0} seconds"));
+            }
+
+           
+
+            //Adds all player notes
+            foreach (Tuple<int, string> playerNote in playerNoteStorage)
+            {
+                __instance.gameStats.allPlayerStats[playerNote.Item1].playerNotes.Add(playerNote.Item2);
+            }
+
+            return true; //Tells patcher to skip orignal code
 
         }
 
